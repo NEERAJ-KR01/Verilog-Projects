9c073ed98ffa06f19420f34d336f4e8f55965c31
NEERAJ-KR01 <neerajkumar9748087@gmail.com>
2026-09-09 01:37:02 +0530

    synchronous FIFO.sv
    Added a testbench for the synchronous FIFO module to verify write and read operations, including overflow handling.

--- synchronous_fifo.sv
+++ synchronous_fifo.sv
@@ -26,5 +26,34 @@ always @(posedge clk or posedge rst)begin
   end
 end
 endmodule
+module tb;
+ reg clk, rst, wr_en,rd_en;
+ reg [7:0]din;
+ wire [7:0]dout;
+wire full,empty;
+
+sync_fifo dut(.*); //system verilog shortcut
+always #5 clk=~clk;
+initial begin
+clk=0; rst=1; din=0; rd_en=0; wr_en=0;
+#15 rst =0;
+$monitor("Time=%0t | rst=%b | wr=%b rd=%b din=%h dout=%h | full=%b empty=%b", 
+ $time, rst, wr_en, rd_en, din, dout, full, empty); 
+   // WRITE till full depth-4
+  @(negedge clk) wr_en=1; din =8'hAA;
+  @(negedge clk) wr_en=1; din =8'hbb;
+  @(negedge clk) wr_en=1; din=8'hCC;
+  @(negedge clk) wr_en=1; din=8'hDD;
+// OVERRIDE more than 4
+  @(negedge clk) wr_en=1; din=8'hEE;
+  @(negedge clk) wr_en=0;
+  // READ till empty
+  @(negedge  clk) rd_en=1; 
+  @(negedge clk);
+ @(negedge clk);
+ @(negedge clk);
+ #20 $finish;
+ end        
+ endmodule   
     
     
